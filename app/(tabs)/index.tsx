3c064e1b40ae021aacb37342df9198fa137b2873
SidLD <cidlibril5@gmail.com>
2025-09-22 09:41:27 +0800

    final

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Platform, StyleSheet, View } from "react-native";
 import NavigationWebView from "../../components/NavigationWebView";
 
 export default function CampusViewer() {
-  return <NavigationWebView />;
+  return (
+    <View style={styles.container}>
+      <NavigationWebView />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? '12%' : '8%',
+    backgroundColor: '#0c0c1a',
+  },
+});
